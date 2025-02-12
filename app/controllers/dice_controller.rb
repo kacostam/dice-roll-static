c9da0b752ef5584e8c9927b0a90aa2325c063dc7
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-02-12 01:50:56 +0000

    created root and 2d10 paths

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,5 +1,14 @@
 class DiceController < ApplicationController
+  
+  def homepage
+    render({:template => "dice_templates/homepage"})
+  end
+  
   def two_six
     render({ :template => "dice_templates/2d6" })
+  end
+
+  def two_ten
+    render({:template => "dice_templates/2d10"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"
-  get("dice/2/6", { :controller =>"dice", :action => "two_six" })  
   
+  # 2d6 path
+  get("dice/2/6", { :controller =>"dice", :action => "two_six" })  
+
+  #2d10 path
+  get("/dice/2/10", {:controller => "dice", :action => "two_ten" } )
+
+  # root path
+  get("/", { :controller => "dice", :action => "homepage"})
+
 end
