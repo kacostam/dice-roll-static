1a24609d014c8540823367d50fb80ad015861975
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-02-12 02:18:31 +0000

    created missing paths and fixed typo in homepage

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -11,4 +11,21 @@ class DiceController < ApplicationController
   def two_ten
     render({:template => "dice_templates/2d10"})
   end
+
+  def one_twenty
+    render( {:template => "dice_templates/1d20"})
+  end 
+
+  def five_four
+    @dice_rolls = Array.new
+
+    5.times do
+      new_roll = rand(1..4)
+
+      @dice_rolls.push(new_roll)
+    end
+
+    render( {:template => "dice_templates/5d4"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,19 @@ Rails.application.routes.draw do
 
   # get "/your_first_screen" => "pages#first"
   
+  # root path
+  get("/", { :controller => "dice", :action => "homepage" })
+
   # 2d6 path
   get("dice/2/6", { :controller =>"dice", :action => "two_six" })  
 
-  #2d10 path
-  get("/dice/2/10", {:controller => "dice", :action => "two_ten" } )
+  # 2d10 path
+  get("/dice/2/10", { :controller => "dice", :action => "two_ten" })
 
-  # root path
-  get("/", { :controller => "dice", :action => "homepage"})
+  # 1d20 path
+  get("/dice/1/20", { :controller => "dice", :action => "one_twenty" })
+
+  # 5d4 path
+  get("/dice/5/4", { :controller => "dice", :action => "five_four"})
 
 end
